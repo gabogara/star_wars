8d28c6ba5f364d193a766e2f82092044c38f914d
gabogara <gabo1gara@gmail.com>
2024-10-06 00:41:24 -0700

    I add title and icon

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import pygame
 pygame.init()
 size=(800,600)
 screen = pygame.display.set_mode(size)
-is_run = True
-while is_run:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-             is_run = False
+
+# Title and Icon
+pygame.display.set_caption("Star wars")
+icon = pygame.image.load("ovni.png")
+pygame.display.set_icon(icon)
+background = pygame.image.load('Fondo.jpg')
